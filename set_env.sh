277a99ab62cfd090adefd593ef8c6f966da0bab6
Scott Medefind <smedefind@optoro.com>
2015-03-19 11:43:52 -0400

    Moving SOLVE_TIMEOUT to proper scripts

--- set_env.sh
+++ set_env.sh
@@ -3,5 +3,3 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export KITCHEN_LOCAL_YAML=~jenkins/.kitchen/config.yml
-# Configure Berkshelf DepSolver timeout to 600 secs
-export SOLVE_TIMEOUT=600

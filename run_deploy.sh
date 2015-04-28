3d802573a82911830ca5da15f2b35aa21e73ae9d
Scott Medefind <smedefind@optoro.com>
2015-04-28 16:09:58 -0400

    Add logging of chef deploys to logstash

--- run_deploy.sh
+++ run_deploy.sh
@@ -7,3 +7,10 @@ $DIR/set_env.sh
 cd $WORKSPACE/repo
 rvm use 2.1.2
 bundle exec berks up
+if [ $? == 0 ];
+then
+  VERSION=`cat $WORKSPACE/repo/metadata.rb | grep -m1 version | sed 's/'\''//g' | awk '{print "v"$2}'`
+  COOKBOOK=`cat $WORKSPACE/repo/metadata.rb | grep -m1 name | sed 's/'\''//g' | cut -d ' ' -f2`
+  # Write log to Elasticsearch via logstash for Grafana annotations
+  echo "{ \"type\": \"chef-deploy\", \"cookbook\": \"${COOKBOOK}\", \"version\": ${VERSION}\" }'" | nc -q1 -u 127.0.0.1 5228
+fi

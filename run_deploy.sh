bf57b9a563ed0f84370ce2e3ce94f02c5b02ffc5
Scott Medefind <smedefind@optoro.com>
2015-04-29 11:01:40 -0400

    Deploy logging missing a "

--- run_deploy.sh
+++ run_deploy.sh
@@ -12,7 +12,7 @@ then
   VERSION=`cat $WORKSPACE/repo/metadata.rb | grep -m1 version | sed 's/'\''//g' | awk '{print "v"$2}'`
   COOKBOOK=`cat $WORKSPACE/repo/metadata.rb | grep -m1 name | sed 's/'\''//g' | cut -d ' ' -f2`
   # Write log to Elasticsearch via logstash for Grafana annotations
-  LOG="{ \"type\": \"chef-deploy\", \"cookbook\": \"${COOKBOOK}\", \"version\": ${VERSION}\", \"cookbook-and-version\": \"${COOKBOOK} - ${VERSION}\" }"
+  LOG="{ \"type\": \"chef-deploy\", \"cookbook\": \"${COOKBOOK}\", \"version\": \"${VERSION}\", \"cookbook-and-version\": \"${COOKBOOK} - ${VERSION}\" }"
   echo "Writing to Logstash: ${LOG}"
   echo $LOG | nc -q1 -u 127.0.0.1 5228
 fi

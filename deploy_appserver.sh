8eb32cd66312d0360351e1a94835fcf0ac04a9ef
Alex Freidah <afreidah@optoro.com>
2015-05-01 15:33:08 -0400

    updated script to make sure system ruby is being used

--- deploy_appserver.sh
+++ deploy_appserver.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env ruby
+#!/bin/bash --login
 
 # get the environment of nodes we want to work with
-environment = ARGV.shift
+environment=$1
 
 # allow a specific pattern to be passed in for node matching, but default to nodes with app if nothing is passed in
-node_pattern = ARGV.shift || "*app*"
+if [ -z "$2" ]; then
+  node_pattern="*app*"
+else
+  node_pattern=$2
+fi
 
-`knife job start 'chef-client' --search 'name:#{node_pattern} AND chef_environment:#{environment}'`
+rvm use system
+knife job start 'chef-client' --search "name:$node_pattern AND chef_environment:$environment"

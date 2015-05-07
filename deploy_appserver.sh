b4e86cf759acf7506ddfe4be720b7b8bdf54af4a
Alex Freidah <afreidah@optoro.com>
2015-05-07 13:27:40 -0400

    updated deploy script to search based on a combination on environment and tag

--- deploy_appserver.sh
+++ deploy_appserver.sh
@@ -2,13 +2,7 @@
 
 # get the environment of nodes we want to work with
 environment=$1
-
-# allow a specific pattern to be passed in for node matching, but default to nodes with app if nothing is passed in
-if [ -z "$2" ]; then
-  node_pattern="*app*"
-else
-  node_pattern=$2
-fi
+tag=$2
 
 rvm use system
-knife job start 'chef-client' --search "name:$node_pattern AND chef_environment:$environment"
+knife job start 'chef-client' --search "chef_environment:$environment AND tags:$tag"

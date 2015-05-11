60e602e8c75f4dc89a7fb5c8427e465848bdee63
Alex Freidah <afreidah@optoro.com>
2015-05-11 15:39:45 -0400

    fixed copy/paste hard coded values that should have been variables

--- deploy_appserver.sh
+++ deploy_appserver.sh
@@ -6,7 +6,7 @@ tag=$2
 action=$3
 
 if [[ "$action" == "rollback" ]]; then
-  for node in `knife search node "chef_environment:stage-blinq AND tags:app" -i -F text | egrep -v "items found|^$"`; do
+  for node in `knife search node "chef_environment:$environment AND tags:$tag" -i -F text | egrep -v "items found|^$"`; do
     echo "setting rollback attribute for node: $node..."
     knife set_attribute node $node one_time_action 'rollback'
   done

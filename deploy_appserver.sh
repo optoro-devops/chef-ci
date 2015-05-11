92f99a694812178004b615d68792b4622529eb97
Alex Freidah <afreidah@optoro.com>
2015-05-11 15:31:16 -0400

    added support for attribute setting in the case of a rollback action being supplied

--- deploy_appserver.sh
+++ deploy_appserver.sh
@@ -3,6 +3,15 @@
 # get the environment of nodes we want to work with
 environment=$1
 tag=$2
+action=$3
+
+if [[ "$action" == "rollback" ]]; then
+  for node in `knife search node "chef_environment:stage-blinq AND tags:app" -i -F text | egrep -v "items found|^$"`; do
+    echo "setting rollback attribute for node: $node..."
+    knife set_attribute node $node one_time_action 'rollback'
+  done
+fi
 
 rvm use system
+echo "beginning $action for nodes..."
 knife job start 'chef-client' --search "chef_environment:$environment AND tags:$tag"

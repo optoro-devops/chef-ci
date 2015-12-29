86ff9d0b8c7fd53f5b30b6800a0c22cd4af6f360
Scott Medefind <smedefind@optoro.com>
2015-12-29 11:11:26 -0500

    Adding chck to verify foodcritic-rules-optoro is being included and run

--- run_foodcritic.sh
+++ run_foodcritic.sh
@@ -6,6 +6,12 @@ $DIR/set_env.sh
 
 cd $WORKSPACE/repo
 rvm use 2.1.2
+grep -s "'foodcritic-rules-optoro'" Gemfile
+if [ $? -ne 0 ]
+then
+  echo -e "ERROR: You need to add foodcritic-rules-optoro to your Gemfile\n\t gem 'foodcritic-rules-optoro'"
+  exit 1
+fi
 if [ -f Thorfile ]; then
   bundle exec foodcritic -f any -B ./ -G
 else
